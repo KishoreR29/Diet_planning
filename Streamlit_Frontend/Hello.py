f706ff469ea677a3159b3e54b0ded9d939bd13af
KishoreR29 <114207813+KishoreR29@users.noreply.github.com>
2023-04-07 18:51:53 +0530

    Update Hello.py

--- Streamlit_Frontend/Hello.py
+++ Streamlit_Frontend/Hello.py
@@ -11,8 +11,8 @@ st.sidebar.success("Select a recommendation app.")
 
 st.markdown(
     """
-    Hi
+
     A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
-    You can find more details and the whole project on my [repo](https://github.com/zakaria-narjis/Diet-Recommendation-System).
+    You can find more details and the whole project on my [repo](https://github.com/KishoreR29/Diet_planning).
     """
 )
